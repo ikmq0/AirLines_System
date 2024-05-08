229ce06c898b638ee5e923dd23080f2b47cc0a41
ikmq0 <71012568+ikmq0@users.noreply.github.com>
2024-05-08 11:02:58 +0300

    Initial commit

--- src/meow.java
+++ src/meow.java
@@ -2,5 +2,6 @@ public class meow {
     public static void main(String[] args) {
         System.out.println("MEowing");
         System.out.println("Abdo");
+        System.out.println("Mhammed");
     }
 }
