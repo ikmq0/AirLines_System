e5a7ef30fef96d076d4f94e58450f4278dfc708a
MamounAli1 <mamounali2005@gmail.com>
2024-05-11 00:38:17 +0300

    meow

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
         int option = 0;
         while (true) {
             System.out.println(CommandColors.BLUE + "1. Book Now" + CommandColors.RESET);
-            System.out.println(CommandColors.BLUE + "2. Manage Booking" + CommandColors.RESET);
+            System.out.println(CommandColors.BLUE + "2. Display Booking" + CommandColors.RESET);
             System.out.println(CommandColors.BLUE + "Select an option: " + CommandColors.RESET);
 
             if (kb.hasNextInt()) {
@@ -25,9 +25,46 @@ public class Main {
                 kb.next();
             }
         }
+
         if (option == 1) {
+            bookFlight(kb);
+        } else if (option == 2) {
+            System.out.println(CommandColors.WHITE + "Please enter your reservation number" + CommandColors.RESET);
+        }
+        
+    }
+
+    public static void bookFlight(Scanner kb) {
+        int departureOption = 0;
+        while (true) {
             System.out.println(CommandColors.WHITE + "Enter Departure City " + CommandColors.RESET);
-            System.out.println(CommandColors.WHITE + " (1) Riyad(RUD) \n (2) Dammam(DMM) \n (3) Jeddah(JDD) \n (4) Madina(MED)" + CommandColors.RESET);
+            System.out.println(CommandColors.WHITE + " (1) Riyadh(RUD) \n (2) Dammam(DMM) \n (3) Jeddah(JDD) \n (4) Madina(MED)" + CommandColors.RESET);
+
+            if (kb.hasNextInt()) {
+                departureOption = kb.nextInt();
+                if (departureOption >= 1 && departureOption <= 4) {
+                    break;
+                } else {
+                    System.out.println(CommandColors.WHITE + "Invalid option. Please enter a number between 1 and 4." + CommandColors.RESET);
+                }
+            } else {
+                System.out.println(CommandColors.WHITE + "Invalid input. Please enter a number." + CommandColors.RESET);
+                kb.next(); // consume invalid input
+            }
+        }
+
+        if (departureOption == 1) {
+            System.out.println(CommandColors.WHITE + "Enter Arrival City " + CommandColors.RESET);
+            System.out.println(CommandColors.WHITE + " (2) Dammam(DMM) \n (3) Jeddah(JDD) \n (4) Madina(MED)" + CommandColors.RESET);
+        } else if (departureOption == 2) {
+            System.out.println(CommandColors.WHITE + "Enter Arrival City " + CommandColors.RESET);
+            System.out.println(CommandColors.WHITE + " (1) Riyadh(RUD) \n (3) Jeddah(JDD) \n (4) Madina(MED)" + CommandColors.RESET);
+        } else if (departureOption == 3) {
+            System.out.println(CommandColors.WHITE + "Enter Arrival City " + CommandColors.RESET);
+            System.out.println(CommandColors.WHITE + " (1) Riyadh(RUD) \n (2) Dammam(DMM) \n (4) Madina(MED)" + CommandColors.RESET);
+        } else if (departureOption == 4) {
+            System.out.println(CommandColors.WHITE + "Enter Arrival City " + CommandColors.RESET);
+            System.out.println(CommandColors.WHITE + " (1) Riyadh(RUD) \n (2) Dammam(DMM) \n (3) Jeddah(JDD)" + CommandColors.RESET);
         }
     }
-    }
+}
